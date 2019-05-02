8088745dd5eb5164a83118b62ebbf6d63b816bde
FezLight <alex_se@hotmail.fr>
2019-05-02 10:32:04 +0200

    DAO factory

--- src/org/enchere/dal/DAOFactory.java
+++ src/org/enchere/dal/DAOFactory.java
@@ -1,9 +1,14 @@
 package org.enchere.dal;
 
+import org.enchere.dal.impl.ArticleVenduHibernateImpl;
 import org.enchere.dal.impl.UtilisateurHibernateImpl;
 
 public abstract class DAOFactory {
 	public static UtilisateurDAO getUtilisateurImpl() {
 		return new UtilisateurHibernateImpl();
+	}
+
+	public static ArticleVenduDAO getArticleVenduImpl() {
+		return new ArticleVenduHibernateImpl();
 	}
 }
