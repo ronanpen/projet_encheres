44248711b9d9f7258edb9b4358f90f05df0c6035
FezLight <alex_se@hotmail.fr>
2019-05-02 02:46:31 +0200

    - Ajout d'implémentation

--- src/org/enchere/dal/DAOFactory.java
+++ src/org/enchere/dal/DAOFactory.java
@@ -2,6 +2,7 @@ package org.enchere.dal;
 
 import org.enchere.dal.impl.ArticleVenduHibernateImpl;
 import org.enchere.dal.impl.CategorieHibernateImpl;
+import org.enchere.dal.impl.EnchereHibernateImpl;
 import org.enchere.dal.impl.UtilisateurHibernateImpl;
 
 public abstract class DAOFactory {
@@ -11,7 +12,10 @@ public abstract class DAOFactory {
 	public static ArticleVenduDAO getArticleVenduImpl() {
 		return new ArticleVenduHibernateImpl();
 	}
-	public static CategorieDAO getCategorieDAO() {
+	public static CategorieDAO getCategorieImpl() {
 		return new CategorieHibernateImpl();
+	}
+	public static EnchereDAO getEnchereImpl() {
+		return new EnchereHibernateImpl();
 	}
 }
