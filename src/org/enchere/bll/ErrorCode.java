2c4d8b565c689167a85891f970da8c930d573a25
FezLight <alex_se@hotmail.fr>
2019-05-02 02:46:21 +0200

    - Ajout code d'erreur

--- src/org/enchere/bll/ErrorCode.java
+++ src/org/enchere/bll/ErrorCode.java
@@ -4,7 +4,8 @@ public enum ErrorCode {
 	MOT_DE_PASSE_NON_IDENTIQUES(1000, "Les mots de passe ne sont pas identiques"),
 	MAIL_INVALIDE(1001, "Le mail ne correspond pas au format standard"),
 	NUMERO_TELEPHONE_INVALIDE(1002, "Le numéro de téléphone n'est pas au bon format (##########)"),
-	CODE_POSTAL_INVALIDE(1003, "Le code postal n'est pas au bon format(#####)");
+	CODE_POSTAL_INVALIDE(1003, "Le code postal n'est pas au bon format(#####)"),
+	PRIX_ENCHERE_INFERIEUR_OU_EGAL_PRIX_ACTUEL(1004, "Prix proposé inférieur ou égal à la valeur actuelle");
 	
 	private int errorCode;
 	private String message = "";
