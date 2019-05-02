b4e517b44bfa7e04864ed4c46fe77296deb9140b
FezLight <alex_se@hotmail.fr>
2019-05-02 02:47:23 +0200

    - Ajout fonctionnalité Retrait

--- src/org/enchere/bo/Retrait.java
+++ src/org/enchere/bo/Retrait.java
@@ -2,6 +2,9 @@ package org.enchere.bo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
@@ -10,6 +13,10 @@ import lombok.Data;
 @Data
 @Entity(name="Retraits")
 public class Retrait {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "no_retrait")
+	private Integer idRetrait;
 	@OneToOne
 	@JoinColumn(name="no_article")
 	@Column(name="no_article")
